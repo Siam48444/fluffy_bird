3e63ee4744c40193df85e64d444545678be7f25c
Siam <siam48444@gmail.com>
2025-03-29 17:40:47 +0600

    siam

---
+++ index.js
@@ -0,0 +1,23 @@
+const gameBoard = document.getElementById('gameBoard');
+
+
+
+
+// Start the game
+document.addEventListener('click', startGame);
+
+function startGame() {
+	poleSlider();
+}
+
+
+// Slides the poles to the left
+function poleSlider() {
+	const POLE_SPEED = 100;
+
+	setTimeout(() => {
+		// Create a pole
+		const pole = document.createElement('div');
+		pole.className = 'pole';
+	}, 5);
+}
