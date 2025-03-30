68ce1bb73cf347a9f3960ba070c394d9c64f5d3b
Siam <siam48444@gmail.com>
2025-03-30 11:10:50 +0600

    siam

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const BOARD = {
 	height : window.innerHeight,
 }
 
+// bird
+const BIRD = {
+	width : 50,
+	height : 50,
+	x : BOARD.width / 3,
+	y : BOARD.height / 2,
+}
+
 
 
 window.onload = () => {
 	const board = document.getElementById('board');
 	const contex = board.getContext('2d'); // used for 2d drawing on the board
 
+	// set the game board dimensions
 	board.width = BOARD.width;
 	board.height = BOARD.height;
 }
